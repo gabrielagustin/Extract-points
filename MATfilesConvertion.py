2fcc09f2a260f0baeb9666a0335a979341ef3bfb
Gabriel Agustin Garcia <gabiagus@gmail.com>
2019-02-07 13:03:52 -0300

    Update MATfilesConvertion.py

--- MATfilesConvertion.py
+++ MATfilesConvertion.py
@@ -3,5 +3,8 @@
 """
 Created on Fri Jan 25 10:16:04 2018
 @author: gag 
+Scrip que lee los archivo .MAT y carga los parametros en un objeto pandas,
+es necesario conocer cuales son las coordenas de los puntos. Luego, crea un archivo
+.KML con los datos requeridos
 """
 
