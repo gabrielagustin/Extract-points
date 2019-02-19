f4c73ae5bed6e943cbbe6a7c4007ff9afc077c5d
gabrielagustin <gabiagus@gmail.com>
2019-02-19 17:53:22 -0300

    add changes

--- read_AMSR2_GW1AM2.py
+++ read_AMSR2_GW1AM2.py
@@ -14,109 +14,179 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import numpy as np
+import pandas as pd
+from shapely.geometry import Point
+
 
 # Reduce font size because dataset's long_name attribute  value is very long.
 mpl.rcParams.update({'font.size': 10})
 
+
+def read_AMSR2_HDF_box(FILE_NAME, box_lat, box_lon, nameVariableArray):
+    """
+    Lee la imagen satelital AMSR2 en formato .H5
+    Recibe el path completo de la image, el box del área específica y las variables a leer
+    A diferencia de la función anterior sólo lee una porción de la imagen satelital,
+    lee el área que recibe en box
+    Retorna un objeto pandas el cual posee como columnas las coordenadas (Lat, Lon)
+    y las variables leidas para cada pixel. 
+    """
+    db=pd.DataFrame()
+    pd.options.mode.chained_assignment = None
+    with h5py.File(FILE_NAME, mode='r') as f:
+        for i in range(0, len(nameVariableArray)):
+            nameVariable = nameVariableArray[i]
+            # print('Variable a extraer:' +str(nameVariable))
+            data = f[nameVariable][:]         
+            # Get the geolocation data
+            if (nameVariable.find("89.0GHz") != -1):        
+                latitude = f['Latitude of Observation Point for 89A'][:]
+                # print(latitude)
+                longitude = f['Longitude of Observation Point for 89A'][:]
+                # print(longitude)
+            else:
+                print("Is NOT at 89.0GHz")
+                #### con AMSR2 la diferencia esta en que para la longitud de onda 89GHz
+                #### posee un submuestreo con respecto a las demas frecuencias
+                latitude = f['Latitude of Observation Point for 89A'][:]
+                # print(latitude)
+                longitude = f['Longitude of Observation Point for 89A'][:]
+                # print(longitude)
+                latitude = latitude[:, ::2]
+                longitude = longitude[:, ::2]
+
+
+            ##### se lee solo el box_lat y box_lon de la variable
+            lat_index = np.logical_and(latitude > box_lat[0], latitude < box_lat[1])
+            lon_index = np.logical_and(longitude > box_lon[0], longitude < box_lon[1])
+            box_index = np.logical_and(lat_index, lon_index)
+            data = f[nameVariable][box_index]
+            #### se genera el objeto pandas
+            db[nameVariable] = data
+            ##### se lee solo el box_lat y box_lon de las coordenadas
+            if (nameVariable.find("89.0GHz") != -1):        
+                latitude = latitude[box_index]
+                longitude = longitude[box_index]
+
+            else:
+                print("Is NOT at 89.0GHz")
+                # latitude = f['Latitude of Observation Point for 89A'][:]
+                # print(latitude)
+                # longitude = f['Longitude of Observation Point for 89A'][:]
+                # print(longitude)
+                # latitude = latitude[:, ::2]
+                # longitude = longitude[:, ::2]
+                latitude = latitude[box_index]
+                longitude = longitude[box_index]
+
+    db["Longitude"] = pd.to_numeric(longitude)
+    db["Latitude"] = pd.to_numeric(latitude)    
+
+    db['Coordinates'] = list(zip(db.Longitude, db.Latitude))
+    db['Coordinates'] = db['Coordinates'].apply(Point)
+
+    db = db.dropna()
+    return db
+
+
+
+
+
+
 def run(FILE_NAME):
 
-    with h5py.File(FILE_NAME, mode='r') as f:
-        print('--------------------------------------------------------------------')
-        print('Names of the groups in HDF5 file:')
-        for key in f.keys():
-            print(key) #Names of the groups in HDF5 file.
-        print('--------------------------------------------------------------------')
-        #Get the HDF5 group
+    # with h5py.File(FILE_NAME, mode='r') as f:
+    #     print('--------------------------------------------------------------------')
+    #     print('Names of the groups in HDF5 file:')
+    #     for key in f.keys():
+    #         print(key) #Names of the groups in HDF5 file.
+    #     print('--------------------------------------------------------------------')
+    #     #Get the HDF5 group
 
-        #### Names of the groups in H5 file:
+    #     #### Names of the groups in H5 file:
 
-        # Brightness Temperature (10.7GHz,H)
-        # Brightness Temperature (10.7GHz,V)
-        # Brightness Temperature (18.7GHz,H)
-        # Brightness Temperature (18.7GHz,V)
-        # Brightness Temperature (23.8GHz,H)
-        # Brightness Temperature (23.8GHz,V)
-        # Brightness Temperature (36.5GHz,H)
-        # Brightness Temperature (36.5GHz,V)
-        # Brightness Temperature (6.9GHz,H)
-        # Brightness Temperature (6.9GHz,V)
-        # Brightness Temperature (7.3GHz,H)
-        # Brightness Temperature (7.3GHz,V)
-        # Brightness Temperature (89.0GHz-A,H)
-        # Brightness Temperature (89.0GHz-A,V)
-        # Brightness Temperature (89.0GHz-B,H)
-        # Brightness Temperature (89.0GHz-B,V)
-        # Cold Sky Mirror Count 6 to 36
-        # Cold Sky Mirror Count 89
-        # Earth Azimuth
-        # Earth Incidence
-        # Hot Load Count 6 to 36
-        # Hot Load Count 89
-        # Interpolation Flag 6 to 36
-        # Interpolation Flag 89
-        # Land_Ocean Flag 6 to 36
-        # Land_Ocean Flag 89
-        # Latitude of Observation Point for 89A
-        # Latitude of Observation Point for 89B
-        # Longitude of Observation Point for 89A
-        # Longitude of Observation Point for 89B
-        # Navigation Data
-        # Observation Supplement
-        # PCD Data
-        # Pixel Data Quality 6 to 36
-        # Pixel Data Quality 89
-        # Position in Orbit
-        # Rx Offset_Gain Count
-        # SPC Temperature Count
-        # SPS Temperature Count
-        # Scan Data Quality
-        # Scan Time
-        # Sun Azimuth
-        # Sun Elevation
+    #     # Brightness Temperature (10.7GHz,H)
+    #     # Brightness Temperature (10.7GHz,V)
+    #     # Brightness Temperature (18.7GHz,H)
+    #     # Brightness Temperature (18.7GHz,V)
+    #     # Brightness Temperature (23.8GHz,H)
+    #     # Brightness Temperature (23.8GHz,V)
+    #     # Brightness Temperature (36.5GHz,H)
+    #     # Brightness Temperature (36.5GHz,V)
+    #     # Brightness Temperature (6.9GHz,H)
+    #     # Brightness Temperature (6.9GHz,V)
+    #     # Brightness Temperature (7.3GHz,H)
+    #     # Brightness Temperature (7.3GHz,V)
+    #     # Brightness Temperature (89.0GHz-A,H)
+    #     # Brightness Temperature (89.0GHz-A,V)
+    #     # Brightness Temperature (89.0GHz-B,H)
+    #     # Brightness Temperature (89.0GHz-B,V)
+    #     # Cold Sky Mirror Count 6 to 36
+    #     # Cold Sky Mirror Count 89
+    #     # Earth Azimuth
+    #     # Earth Incidence
+    #     # Hot Load Count 6 to 36
+    #     # Hot Load Count 89
+    #     # Interpolation Flag 6 to 36
+    #     # Interpolation Flag 89
+    #     # Land_Ocean Flag 6 to 36
+    #     # Land_Ocean Flag 89
+    #     # Latitude of Observation Point for 89A
+    #     # Latitude of Observation Point for 89B
+    #     # Longitude of Observation Point for 89A
+    #     # Longitude of Observation Point for 89B
+    #     # Navigation Data
+    #     # Observation Supplement
+    #     # PCD Data
+    #     # Pixel Data Quality 6 to 36
+    #     # Pixel Data Quality 89
+    #     # Position in Orbit
+    #     # Rx Offset_Gain Count
+    #     # SPC Temperature Count
+    #     # SPS Temperature Count
+    #     # Scan Data Quality
+    #     # Scan Time
+    #     # Sun Azimuth
+    #     # Sun Elevation
 
-        # name = 'Brightness Temperature (10.7GHz,H)'
-        # name = 'Brightness Temperature (18.7GHz,V)'
-        # name = 'Brightness Temperature (6.9GHz,V)'
-        # name = 'Brightness Temperature (7.3GHz,H)'
-        # name = 'Brightness Temperature (36.5GHz,H)'     
-        name = 'Brightness Temperature (23.8GHz,V)'  
-        # name = "Brightness Temperature (89.0GHz-A,H)"
-        # name = 'Brightness Temperature (89.0GHz-A,V)'
+  
+    #     # name = "Brightness Temperature (89.0GHz-A,H)"
+    #     # name = 'Brightness Temperature (89.0GHz-A,V)'
 
-        data = f[name][:]
-        print(data)
-        print(data.shape)
-        # longname = f[name].attrs['long_name']
-        # _FillValue = f[name].attrs['_FillValue']
-        # valid_max = f[name].attrs['valid_max']
-        # valid_min = f[name].attrs['valid_min']        
-        # invalid = np.logical_or(data > valid_max,
-        #                     data < valid_min)
-        # invalid = np.logical_or(invalid, data == _FillValue)
-        # data[invalid] = np.nan
-        # data = np.ma.masked_where(np.isnan(data), data)
+    #     data = f[name][:]
+    #     print(data)
+    #     print(data.shape)
+    #     # longname = f[name].attrs['long_name']
+    #     # _FillValue = f[name].attrs['_FillValue']
+    #     # valid_max = f[name].attrs['valid_max']
+    #     # valid_min = f[name].attrs['valid_min']        
+    #     # invalid = np.logical_or(data > valid_max,
+    #     #                     data < valid_min)
+    #     # invalid = np.logical_or(invalid, data == _FillValue)
+    #     # data[invalid] = np.nan
+    #     # data = np.ma.masked_where(np.isnan(data), data)
         
-        # Get the geolocation data
+    #     # Get the geolocation data
 
-        if (name.find("89.0GHz") != -1):        
-            latitude = f['Latitude of Observation Point for 89A'][:]
-            print(latitude)
-            longitude = f['Longitude of Observation Point for 89A'][:]
-            print(longitude)
-        else:
-            latitude = f['Latitude of Observation Point for 89A'][:]
-            print(latitude)
-            longitude = f['Longitude of Observation Point for 89A'][:]
-            print(longitude)
-            latitude = latitude[:, ::2]
-            longitude = longitude[:, ::2]
+    #     if (name.find("89.0GHz") != -1):        
+    #         latitude = f['Latitude of Observation Point for 89A'][:]
+    #         print(latitude)
+    #         longitude = f['Longitude of Observation Point for 89A'][:]
+    #         print(longitude)
+    #     else:
+    #         latitude = f['Latitude of Observation Point for 89A'][:]
+    #         print(latitude)
+    #         longitude = f['Longitude of Observation Point for 89A'][:]
+    #         print(longitude)
+    #         latitude = latitude[:, ::2]
+    #         longitude = longitude[:, ::2]
         
-    m = Basemap(projection='cyl', resolution='l',
-               llcrnrlat=-90, urcrnrlat=90,
-               llcrnrlon=-180, urcrnrlon=180)
-    # # m = Basemap(projection='cyl', resolution='l',
-    # #            llcrnrlat= -90, urcrnrlat=-40,
-    # #            llcrnrlon=-180, urcrnrlon=180)
+    # m = Basemap(projection='cyl', resolution='l',
+    #            llcrnrlat=-90, urcrnrlat=90,
+    #            llcrnrlon=-180, urcrnrlon=180)
+    # # # m = Basemap(projection='cyl', resolution='l',
+    # # #            llcrnrlat= -90, urcrnrlat=-40,
+    # # #            llcrnrlon=-180, urcrnrlon=180)
 
     # box_lat = [-90, -60]
     # box_lon = [105, 180]
@@ -131,19 +201,37 @@ def run(FILE_NAME):
     # m = Basemap(projection='cyl', resolution='l',
     #             llcrnrlat=-86.88, urcrnrlat=-64.54,
     #             llcrnrlon=119.97, urcrnrlon=176.019)
-    m.drawcoastlines(linewidth=0.5)
-    m.drawparallels(np.arange(-90, 91, 10),labels=[True,False,False,True])
-    m.drawmeridians(np.arange(-180, 180, 15), labels=[True,False,False,True])
-    m.scatter(longitude, latitude, c=data, s=1, cmap=plt.cm.jet,
-            edgecolors=None, linewidth=0)
-    cb = m.colorbar(location="bottom", pad=0.7)    
-    cb.set_label('[°K]')
 
-    # basename = os.path.basename(FILE_NAME)
 
-    # plt.title('{0}\n{1}'.format(basename, longname))
-    #fig = plt.gcf()
-    plt.show()
+    box_lat = [-85, -65]
+    box_lon = [120, 180]
+
+    # nameVariableArray = ['Brightness Temperature (10.7GHz,H)', 'Brightness Temperature (10.7GHz,V)', 'Brightness Temperature (18.7GHz,H)',
+    # 'Brightness Temperature (18.7GHz,V)', 'Brightness Temperature (23.8GHz,H)', 'Brightness Temperature (23.8GHz,V)',
+    # 'Brightness Temperature (36.5GHz,H)', 'Brightness Temperature (36.5GHz,V)', 'Brightness Temperature (6.9GHz,H)',
+    # 'Brightness Temperature (6.9GHz,V)', 'Brightness Temperature (7.3GHz,H)', 'Brightness Temperature (7.3GHz,V)']
+
+
+    nameVariableArray = ['Brightness Temperature (89.0GHz-B,V)', 'Brightness Temperature (89.0GHz-B,H)']
+    df = read_AMSR2_HDF_box(FILE_NAME, box_lat, box_lon, nameVariableArray)
+
+    print(df)
+    # longitude = df['Longitude']
+
+
+    # m.drawcoastlines(linewidth=0.5)
+    # m.drawparallels(np.arange(-90, 91, 10),labels=[True,False,False,True])
+    # m.drawmeridians(np.arange(-180, 180, 15), labels=[True,False,False,True])
+    # m.scatter(longitude, latitude, c=data, s=1, cmap=plt.cm.jet,
+    #         edgecolors=None, linewidth=0)
+    # cb = m.colorbar(location="bottom", pad=0.7)    
+    # cb.set_label('[°K]')
+
+    # # basename = os.path.basename(FILE_NAME)
+
+    # # plt.title('{0}\n{1}'.format(basename, longname))
+    # #fig = plt.gcf()
+    # plt.show()
 #    pngfile = "{0}.py.png".format(basename)
 #    fig.savefig(pngfile)
 
@@ -159,4 +247,3 @@ if __name__ == "__main__":
         pass
 
     run(hdffile)
-
