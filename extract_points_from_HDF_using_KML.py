3d0cfc99652a993e34d5e198db58a79cf31732a6
gabrielagustin <gabiagus@gmail.com>
2019-02-20 08:27:48 -0300

    add changes

--- extract_points_from_HDF_using_KML.py
+++ extract_points_from_HDF_using_KML.py
@@ -32,27 +32,48 @@ if __name__ == "__main__":
         if file.endswith(".h5"):
             print(file)
             hdfFile = os.path.join(mydir, file)
+            print('/home/gag/Escritorio/Extract/'+ file[:-3]+'.csv')
             # print(hdfFile)
+            # If a certain environment variable is set, look there for the input
+            # file, otherwise look in the current directory.
+            # hdfFile = '/home/gag/Escritorio/SMAP_L1B/SMAP_L1B_TB_20137_A_20181108T101842_R16020_001.h5'
             try:
                 hdfFile = os.path.join(os.environ['HDFEOS_ZOO_DIR'], hdfFile)
             except KeyError:
                 pass
-           
-            #### el nombre de las variables se forma con # nameVariable = group + nameVariable
-            # nameVariable = '/Brightness_Temperature/tb_h'
-            # print('Variable a extraer: '+str(nameVariable))
-            nameVariableArray = ['/Brightness_Temperature/tb_h', '/Brightness_Temperature/tb_v', '/Brightness_Temperature/toa_h', '/Brightness_Temperature/toa_v']
+
+            ##### lee el archivo HDF, se extraen las variables  y se genera el objeto geopandas
             ####-------------------------------------------------------------------------------------
             #### lectura de la imagen H5 completa
+            # nameVariable = '/Brightness_Temperature/tb_h'
+            # print('Variable a extraer: '+str(nameVariable))
             # pdHDF = functions.readHDF(hdfFile, nameVariableArray)
             ####-------------------------------------------------------------------------------------
             #### lectura de un subset de la imagen H5 
+            # box_lat = [-90, -60]
+            # box_lon = [105, 180]
             box_lat = [-85, -65]
             box_lon = [120, 180]
-            pdHDF = functions.readHDF_box(hdfFile, box_lat, box_lon, nameVariableArray)
+            ####-------------------------------------------------------------------------------------
+            #### se utilizan distintas funciones de lectura debido al diferente almacenamiento
+            #### de las variables y las coordendas (Lat y Lon)
+            ####-------------------------------------------------------------------------------------
+            ### para las imágenes satelitales SMAP L1B
+            ####-------------------------------------------------------------------------------------
+            ### para SMAPL1B el nombre de las variables se forma con # nameVariable = group + nameVariable
+            # nameVariableArray = ['/Brightness_Temperature/tb_h', '/Brightness_Temperature/tb_v', '/Brightness_Temperature/toa_h', '/Brightness_Temperature/toa_v']
+            # pdHDF = functions.read_SMAP_L1B_HDF_box(hdfFile, box_lat, box_lon, nameVariableArray)
+            ####-------------------------------------------------------------------------------------
+            ### para las imágenes satelitales AMSR2
+            ####-------------------------------------------------------------------------------------
+            nameVariableArray = ['Brightness Temperature (10.7GHz,H)', 'Brightness Temperature (10.7GHz,V)', 'Brightness Temperature (18.7GHz,H)',
+            'Brightness Temperature (18.7GHz,V)', 'Brightness Temperature (23.8GHz,H)', 'Brightness Temperature (23.8GHz,V)',
+            'Brightness Temperature (36.5GHz,H)', 'Brightness Temperature (36.5GHz,V)', 'Brightness Temperature (6.9GHz,H)',
+            'Brightness Temperature (6.9GHz,V)', 'Brightness Temperature (7.3GHz,H)', 'Brightness Temperature (7.3GHz,V)']
+            pdHDF = functions.read_AMSR2_HDF_box(hdfFile, box_lat, box_lon, nameVariableArray)
 
 
-            #### genera objeto geopandas con la imagen H5
+
             gdfHDF = geopandas.GeoDataFrame(pdHDF, geometry='Coordinates')
             # print(gdfHDF)
             print('Variables leidas del archivo HDF:')
@@ -68,7 +89,7 @@ if __name__ == "__main__":
                 name = "flight_"+str(i)
                 print("Vuelo: " +str(name))
                 ##### lee el archivo KML y se crea un objeto geopandas
-                kmlFile = "/.../".kml"
+                kmlFile = "/home/gag/Escritorio/Lineas_de_vuelo_Antartida/2018_UWBRAD_"+name+".kml"
                 pdKML = functions.readKML(kmlFile)
                 gdfKML = geopandas.GeoDataFrame(pdKML, geometry='Coordinates')
 
@@ -98,10 +119,11 @@ if __name__ == "__main__":
                 geosResult['Point_name'] = namePointArray
                 geosResult['Distance[degree]'] = distanceArray
                 # print(list(geosResult))
-       
+            # geosResult =geosResult[["Point_name","Coordinates_KML","Coordinates_HDF", "/Brightness_Temperature/tb_h", "/Brightness_Temperature/tb_v", "/Brightness_Temperature/toa_h", "/Brightness_Temperature/toa_v"]] 
             geosResult =geosResult[["Point_name","Coordinates_KML","Coordinates_HDF","Distance[degree]", "/Brightness_Temperature/tb_h", "/Brightness_Temperature/tb_v", "/Brightness_Temperature/toa_h", "/Brightness_Temperature/toa_v"]] 
-            geosResult.to_csv('/.../'+'.csv', decimal = ",", index= False)
+            geosResult.to_csv('/home/gag/Escritorio/Extract/'+ file[:-3]+'.csv', decimal = ",", index= False)
             print("Archivo creado con exito!")
             elapsed_time = time() - start_time
             print("Elapsed time: %.10f seconds." % elapsed_time)
+
 
