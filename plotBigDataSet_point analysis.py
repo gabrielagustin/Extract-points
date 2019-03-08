dd3719791eb1898d7a2b3f604dd88a7532adceeb
gabrielagustin <gabiagus@gmail.com>
2019-03-08 10:20:47 -0300

    add changes

--- plotBigDataSet_point analysis.py
+++ plotBigDataSet_point analysis.py
@@ -34,23 +34,23 @@ df['Distance[degree]'] = (df['Distance[degree]']/360)*np.pi*12756.2
 #### se agrupa por: Fecha y Coordenadas y se calcula la media de las variables. Es decir, se calcula la
 #### media para cada dia de cada punto o lo que es lo mismo, se promedia las pasadas del satelite en el día.
  
-group_data = df.groupby(['Date','Coordinates_KML'], sort=False)['/Brightness_Temperature/tb_h', '/Brightness_Temperature/tb_v'].mean()
-# print(group_data)
+group_data1 = df.groupby(['Date','Coordinates_KML'], sort=False)['/Brightness_Temperature/tb_h', '/Brightness_Temperature/tb_v'].mean()
+# print(group_data1)
+
 
 #### Luego, se vuelve a agrupar por punto (coordinates) para todas las fechas y se calcula la media y el STD
-group_data = df.groupby(['Coordinates_KML'], sort=False)['/Brightness_Temperature/tb_h', '/Brightness_Temperature/tb_v'].agg({'mean', 'std'})
+group_data2 = group_data1.groupby(['Coordinates_KML'], sort=False)['/Brightness_Temperature/tb_h', '/Brightness_Temperature/tb_v'].agg({'mean', 'std'})
 
-print(group_data)
+print(group_data2)
 
-df1 = group_data
-df1 = df1.reset_index(drop=False)
-print(df1)
+group_data2 = group_data2.reset_index(drop=False)
+print(group_data2)
 #### se genera un nuevo archivo .CSV con los estadisticos por punto
-df1.to_csv("/home/gag/Escritorio/DataSet_Dec_Flight_Dec_mean_std.csv", decimal = ",")
+group_data2.to_csv("/home/gag/Escritorio/DataSet_Dec_Flight_Dec_mean_std.csv", decimal = ".")
 
 #### se genera la grafica
 # fig1, ax1 = plt.subplots()
-ax1= df1.plot(style='.')
+ax1= group_data2.plot(style='.')
 plt.title('SMAP L1B - December Flight Line December')
 ax1.set_xlabel("Point")
 ax1.set_ylabel("Brightness_Temperature[K]")
